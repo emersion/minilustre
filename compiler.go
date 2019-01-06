346f9c463ed88f1b21c2f606b924dc721c526eb7
emersion <contact@emersion.fr>
2019-01-06 23:27:26 +0100

    Add support for variables and some binops to compiler

--- compiler.go
+++ compiler.go
@@ -15,7 +15,28 @@ type compiler struct {
 	funcs map[string]*ir.Func
 }
 
-func (c *compiler) expr(e Expr, blk *ir.Block) (value.Value, error) {
+type context struct {
+	b *ir.Block
+	vars map[string]value.Value
+}
+
+func (c *compiler) typ(t Type) types.Type {
+	switch t {
+	case TypeUnit:
+		return types.Void
+	case TypeBool:
+		return types.I1
+	case TypeInt:
+		return types.I32
+	case TypeFloat:
+		return types.Float
+	case TypeString:
+		return types.I8Ptr
+	}
+	panic(fmt.Sprintf("unknown type %v", t))
+}
+
+func (c *compiler) expr(e Expr, ctx *context) (value.Value, error) {
 	switch e := e.(type) {
 	case *ExprCall:
 		f, ok := c.funcs[e.Name]
@@ -25,50 +46,99 @@ func (c *compiler) expr(e Expr, blk *ir.Block) (value.Value, error) {
 		args := make([]value.Value, len(e.Args))
 		for i, arg := range e.Args {
 			var err error
-			args[i], err = c.expr(arg, blk)
+			args[i], err = c.expr(arg, ctx)
 			if err != nil {
 				return nil, err
 			}
 		}
-		return blk.NewCall(f, args...), nil
+		return ctx.b.NewCall(f, args...), nil
 	case ExprConst:
 		switch v := e.Value.(type) {
+		case int:
+			return constant.NewInt(types.I32, int64(v)), nil
 		case string:
 			b := append([]byte(v), 0)
 			glob := c.m.NewGlobalDef("", constant.NewCharArray(b))
 			glob.Linkage = enum.LinkagePrivate
 			zero := constant.NewInt(types.I64, 0)
-			ptr := blk.NewGetElementPtr(glob, zero, zero)
+			ptr := ctx.b.NewGetElementPtr(glob, zero, zero)
 			return ptr, nil
 		default:
 			panic(fmt.Sprintf("unknown const type %T", v))
 		}
+	case ExprVar:
+		v, ok := ctx.vars[string(e)]
+		if !ok {
+			//panic(fmt.Sprintf("referring to undefined variable '%v'", string(e)))
+			return nil, fmt.Errorf("minilustre: referring to undefined variable '%v'", string(e))
+		}
+		return v, nil
+	case *ExprBinOp:
+		left, err := c.expr(e.Left, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		right, err := c.expr(e.Right, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		switch e.Op {
+		case BinOpMinus:
+			return ctx.b.NewSub(left, right), nil
+		case BinOpPlus:
+			return ctx.b.NewAdd(left, right), nil
+		case BinOpGt:
+			return ctx.b.NewICmp(enum.IPredSGT, left, right), nil
+		case BinOpLt:
+			return ctx.b.NewICmp(enum.IPredSLT, left, right), nil
+		case BinOpFby:
+			return constant.NewInt(types.I32, 0), nil // TODO
+		default:
+			panic(fmt.Sprintf("unknown binary operation %v", e.Op))
+		}
 	default:
-		panic("minilustre: unknown expression")
+		panic(fmt.Sprintf("unknown expression %T", e))
 	}
 }
 
-func (c *compiler) assign(assign *Assign, b *ir.Block) error {
-	_, err := c.expr(assign.Body, b)
+func (c *compiler) assign(assign *Assign, ctx *context) error {
+	v, err := c.expr(assign.Body, ctx)
 	if err != nil {
 		return err
 	}
 
-	// TODO
+	if _, ok := ctx.vars[assign.Dst[0]]; ok {
+		return fmt.Errorf("minilustre: cannot write variable '%v' twice", assign.Dst[0])
+	}
+
+	ctx.vars[assign.Dst[0]] = v
 	return nil
 }
 
 func (c *compiler) node(n *Node) error {
-	f := c.m.NewFunc(n.Name, types.Void)
+	vars := make(map[string]value.Value, len(n.InParams))
+	params := make([]*ir.Param, 0, len(n.InParams))
+	for name, typ := range n.InParams {
+		p := ir.NewParam(name, c.typ(typ))
+		params = append(params, p)
+		vars[name] = p
+	}
+
+	f := c.m.NewFunc(n.Name, types.Void, params...)
 	entry := f.NewBlock("")
 
+	ctx := context{entry, vars}
 	for _, assign := range n.Body {
-		if err := c.assign(&assign, entry); err != nil {
-			return err
+		if err := c.assign(&assign, &ctx); err != nil {
+			return fmt.Errorf("failed to compile node '%v': %v", n.Name, err)
 		}
 	}
 
 	entry.NewRet(nil)
+
+	c.funcs[n.Name] = f
 	return nil
 }
 
